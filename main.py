a2c62c28d46b82962e46ff33a24febc7e747ab61
Pliszka <karoliniakkjk@gmail.com>
2022-03-16 21:59:30 +0100

    Checking time

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-PATHS = ['//*[@id="buyTime machine"]/b/text()[2]',
-         '//*[@id="buyPortal"]/b/text()[2]',
-         '//*[@id="buyAlchemy lab"]/b/text()[2]',
+PATHS = ['#buyMine b',
+         '#buyPortal b',
+         '#by',
          '//*[@id="buyShipment"]/b/text()[2]',
          '//*[@id="buyMine"]/b/text()[2]',
          '//*[@id="buyFactory"]/b/text()[2]',
@@ -13,8 +13,8 @@ PATHS = ['//*[@id="buyTime machine"]/b/text()[2]',
 
 
 def enoughMoney(money, price):
-    price_block = driver.find_element(By.XPATH, price)
-    if int(money.text) >= int(price_block.text):
+    price_block = driver.find_element(By.CSS_SELECTOR, price)
+    if int(money.text) >= int(price_block.text[1]):
         print(money.text, price_block.text)
         price_block.click()
 
@@ -22,12 +22,11 @@ def enoughMoney(money, price):
 chrome_driver_path = r'C:\Users\pluus\dev_stuff\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get('http://orteil.dashnet.org/experiments/cookie/')
-timecheck = time.time()
+timecheck = int(time.time())
 while True:
-    print(timecheck)
     big_cookie = driver.find_element(By.ID, 'cookie')
     big_cookie.click()
     cookies = driver.find_element(By.ID, 'money')
-    if timecheck % 5 == 0:
+    if (int(time.time()) - timecheck) % 5 == 0:
         for path in PATHS:
             enoughMoney(cookies, path)
